159732d94eec7254e45120b065154889e66c88db
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-24 17:07:47 +0400

    now signin signout works but when on resources and signing out, it doesn't redirect to home

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -9,8 +9,11 @@ export default function(ComposedComponent) {
 		};
 
 		componentWillMount() {
-			this.context.router.push('/');
+			if(!this.props.authenticated) {
+				this.context.router.push('/');
+			}
 		}
+
 
 		render() {
 			return(
